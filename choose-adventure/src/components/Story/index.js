3f68a53e3e7a6bfa90cdec9b541256212b691642
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-02-16 20:23:05 -0500

    Worked on json objects for plot points, added beginner logic to Story/index.js

--- choose-adventure/src/components/Story/index.js
+++ choose-adventure/src/components/Story/index.js
@@ -1,28 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import plot from "../../utils/plot";
 import './style.css';
 
-function Story(props){
-    const [count, setCount] = useState(0);
-
-    function nextStep(){
-        setCount(count + 1);
+function Story(){
+    console.log(plot);
+    var currentNode = plot[0];
+    function createChoices(choice) {
+        return(
+            <button>{choice.text}</button>
+        )
     }
 
-    function prevStep(){
-        setCount(count - 1);
+    function getCurrentNode() {
+        return currentNode;
+    }
+
+    function setCurrentNode(targetId) {
+        currentNode = plot.find(x => x.id === targetId)
     }
 
     return(
         <div className="story-div">
-            <h2 id="plotTitle">{plot[count].title}</h2>
-            <p id="plotContent">{plot[count].content}</p>
+            <h2 id="plotTitle"></h2>
+            <p id="plotContent"></p>
             <div className="choice-div">
-                <button onClick={nextStep} className="btn-light btn choice-btn">{plot[count].choice1}</button>
-                <button onClick={prevStep} className="btn-light btn choice-btn">{plot[count].choice2}</button>
+                {plot.choices.map(choice => (
+                    <button>{choice.text}</button>
+                ))}
             </div>
         </div>
     )
 }
 
 export default Story;
+
+
+// {plot.choices.map(createChoices)}
