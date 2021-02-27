d4ad640bf319d5a8f9465b4e4c0c8ba3fc82c9ed
Em <emilylauder@gmail.com>
2021-02-27 11:47:33 -0500

    updated imports and plot edits

--- choose-adventure/client/src/components/Pages/Story/index.js
+++ choose-adventure/client/src/components/Pages/Story/index.js
@@ -1,50 +1,51 @@
 import React from "react";
-import plot from "../../utils/plot";
-import './style.css';
+import plot from "../../../utils/plotbackup";
+import "./style.css";
 
-function Story(){
-    const [node, setNode] = React.useState(0);
-    const [target, setTarget] = React.useState(plot[0].target);
-    
-    var currentNode = plot[node];
+function Story() {
+  const [node, setNode] = React.useState(0);
+  const [target, setTarget] = React.useState(plot[0].target);
 
-    function getCurrentNode() {
-        return currentNode;
-    }
+  var currentNode = plot[node];
 
-    function setCurrentNode(targetId) {
-        currentNode = plot.find(x => x.id === targetId);
-        return currentNode;
-    }
+  function getCurrentNode() {
+    return currentNode;
+  }
 
-    function onClick(choice) {
-        console.log(choice.text);
-        console.log("Target: " + choice.target);
-        setNode(choice.target);
-        setCurrentNode(choice.target);
-        console.log(currentNode);
-        setTarget(currentNode.id);
-        getCurrentNode();
-        // console.log(choice.text);
-        // console.log("Target ID: " + choice.target);
-        // setCurrentNode(choice.target);
-    }
+  function setCurrentNode(targetId) {
+    currentNode = plot.find((x) => x.id === targetId);
+    return currentNode;
+  }
 
-    return(
-        <div className="story-div">
-            <h2 id="plotTitle">{currentNode.title}</h2>
-            <p id="plotContent">{currentNode.content}</p>
-            <div className="choice-div">
-                {currentNode.choices.map(choice => (
-                    <button value={choice.id} onClick={() => onClick(choice)}>{choice.text}</button>
-                ))}
-            </div>
-        </div>
-    )
+  function onClick(choice) {
+    console.log(choice.text);
+    console.log("Target: " + choice.target);
+    setNode(choice.target);
+    setCurrentNode(choice.target);
+    console.log(currentNode);
+    setTarget(currentNode.id);
+    getCurrentNode();
+    // console.log(choice.text);
+    // console.log("Target ID: " + choice.target);
+    // setCurrentNode(choice.target);
+  }
+
+  return (
+    <div className="story-div">
+      <h2 id="plotTitle">{currentNode.title}</h2>
+      <p id="plotContent">{currentNode.content}</p>
+      <div className="choice-div">
+        {currentNode.choices.map((choice) => (
+          <button value={choice.id} onClick={() => onClick(choice)}>
+            {choice.text}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Story;
-
 
 // {plot.choices.map(createChoices)}
 //
