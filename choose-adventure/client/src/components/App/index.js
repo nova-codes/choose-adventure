52f812e903f80a26c51eb7e04dee3e8663ad73ab
Em <emilylauder@gmail.com>
2021-02-28 18:02:39 -0500

    editing nav styles

--- choose-adventure/client/src/components/App/index.js
+++ choose-adventure/client/src/components/App/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import './style.css';
+import "./style.css";
 
-import PrivateRoute from '../../hocs/PrivateRoute';
+import PrivateRoute from "../../hocs/PrivateRoute";
 
-import Story from '../Pages/Story';
-import NavBar from '../NavBar';
-import Login from '../Pages/Login';
-import Admin from '../Pages/Admin';
+import Story from "../Pages/Story";
+import NavBar from "../NavBar";
+import Login from "../Pages/Login";
+import Admin from "../Pages/Admin";
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Switch>
-        <Route exact path="/" component={Story}/>
-        <Route exact path="/login" component={Login}/>
-        <PrivateRoute path="/admin" component={Admin}/>
+        <Route exact path="/" component={Story} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute path="/admin" component={Admin} />
       </Switch>
     </Router>
   );
